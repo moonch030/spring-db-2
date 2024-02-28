93c3bdad003db6d4f79f180bce7bc35b52e0dd6c
moon <cogus2010@naver.com>
2024-02-28 21:03:59 +0900

    2024-02-28 프록시 내부 호출1

--- src/test/java/hello/springtx/apply/InternalCallV1Test.java
+++ src/test/java/hello/springtx/apply/InternalCallV1Test.java
@@ -1,44 +1,56 @@
 package hello.springtx.apply;
 
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.transaction.annotation.Transactional;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
 
+@Slf4j
 @SpringBootTest
-public class TxLevelTest {
+public class InternalCallV1Test {
 
-    @Autowired LevelService service;
+    @Autowired CallService callService;
 
     @Test
-    void orderTest(){
-        service.write();
-        service.read();
+    void printProxy(){
+        log.info("callService class={}", callService.getClass());
+    }
+
+    @Test
+    void internalCall(){
+        callService.internal();
+    }
+
+    @Test
+    void externalCall(){
+        callService.external();
     }
 
     @TestConfiguration
-    static class TxLevelTestConfig{
+    static class InternalCallV1TestConfig{
+
+
         @Bean
-        LevelService levelService(){
-            return new LevelService();
+        CallService callService(){
+            return new CallService();
         }
     }
     @Slf4j
-    @Transactional(readOnly = true)
-    static class LevelService{
+    static class CallService{
 
-        @Transactional(readOnly = false)
-        public void write(){
-            log.info("call write");
+        public void external(){
+            log.info("call external");
             printTxInfo();
+            internal();
         }
 
-        public void read(){
-            log.info("call read");
+        @Transactional
+        public void internal(){
+            log.info("call internal");
             printTxInfo();
         }
 
